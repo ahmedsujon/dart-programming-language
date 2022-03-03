8be0da6293c4fc160240ab275e6ec0cd5e96153e
ahmedsujon <sujonahmed424@gmail.com>
2022-03-03 12:02:08 +0600

    Dart Variables

--- bin/main.dart
+++ bin/main.dart
@@ -1,6 +1,12 @@
 void main(){
 
-  var fName = "Sujon";
-  var lName = "Ahmed";
-  print(fName+" "+lName);
+  int a = 100;
+  int b = 200;
+  int c = 300;
+
+  print(a+b+c);
+  print(a-b+c);
+  print((a*b)+c);
+  print((a*b)/c);
+
 }

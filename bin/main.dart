5ccab7a07f12659b7d799bd691c6a94bcd1b3e85
ahmedsujon <sujonahmed424@gmail.com>
2022-03-03 13:04:15 +0600

    Removing List items

--- bin/main.dart
+++ bin/main.dart
@@ -2,7 +2,14 @@ void main(){
 
 List<String> mylist = [ ];
 
-  mylist.insertAll(0, ["a", "b", "c"]);
+  mylist.add("a");
+  mylist.add("b");
+  mylist.add("c");
+  mylist.add("d");
+
+
+  mylist.removeAt(2);
+  mylist.removeAt(2);
   
 
   print(mylist);

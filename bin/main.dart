d5d09031953a9157c2d0b89c6868f075357d42a6
ahmedsujon <sujonahmed424@gmail.com>
2022-03-03 12:58:56 +0600

    Inserting Elements into a List

--- bin/main.dart
+++ bin/main.dart
@@ -1,16 +1,10 @@
 void main(){
 
-    var myList = [25, 63, 84];  
-    print(myList);
-     
-    myList.add(94);
-    myList.add(95);
-    myList.add(96);
-    myList.add(95);
-    myList.add(96);
-    myList.add(96);
+List<String> mylist = [ ];
 
+  mylist.insertAll(0, ["a", "b", "c"]);
+  
 
-    print(myList);
+  print(mylist);
 
 }
